ngvoice/sems-amr

commit 263845ad5d607a37b004e62a9959a0406a784c3f

AmMediaProcessorThread: most of the put/get functionality moved outside
AmMediaProcessorThread now works just with quite limited interface
(AmMediaSession) instead of directly manipulating AmSession members. This
allows better control over media streams.

core/AmSession.cpp
@@ -63,7 +63,7 @@ int AmAudioPair::process(unsigned int ts, unsigned char *buffer, AmSession *dtmf
   if (checkInterval(ts)) {
     unsigned int f_size = getFrameSize();
     int got = 0;
-    if (src) src->get(ts, buffer, f_size);
+    if (src) got = src->get(ts, buffer, f_size);
     if (got < 0) return -1;
     if (got > 0) {
       if (handle_dtmf && dtmf_handler)
@@ -82,30 +82,6 @@ bool AmAudioPair::checkInterval(unsigned int ts)
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
-    
-int AmAudioSession::processMedia(bool write_streams, unsigned int ts, unsigned char *buffer)
-{
-  int res = 0;
-  AmSession *dtmf_handler = 0;
-
-  session->lockAudio();
-
-  if (session->isDtmfDetectionEnabled()) dtmf_handler = session;
-  for (vector<AmAudioPair>::iterator i = streams.begin(); i != streams.end(); ++i) {
-    if (write_streams != i->isOutput()) continue;
-    if (i->process(ts, buffer, dtmf_handler) < 0) {
-      res = -1;
-      break;
-    }
-  }
-
-  session->unlockAudio();
-
-  return res;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////
-
 
 volatile unsigned int AmSession::session_num = 0;
 AmMutex AmSession::session_num_mut;
@@ -132,7 +108,6 @@ AmSession::AmSession()
     dlg(this),
     processing_media(false),
     sess_stopped(false),
-    input(0), output(0), local_input(0), local_output(0),
     m_dtmfDetector(this), m_dtmfEventQueue(&m_dtmfDetector),
     m_dtmfDetectionEnabled(true),
     accept_early_session(false),
@@ -147,8 +122,6 @@ AmSession::AmSession()
   , _pid(this)
 #endif
 {
-  use_local_audio[AM_AUDIO_IN] = false;
-  use_local_audio[AM_AUDIO_OUT] = false;
 }
 
 AmSession::~AmSession()
@@ -185,9 +158,9 @@ void AmSession::startMediaProcessing()
   if(getStopped() || processing_media.get())
     return;
 
-  bool in_out_set = false;
+  bool in_out_set;
   lockAudio();
-  in_out_set = input || output || local_input || local_output;
+  in_out_set = streams.size() > 0;
   unlockAudio();
   if(in_out_set) {
     AmMediaProcessor::instance()->addSession(this, callgroup);
@@ -215,56 +188,75 @@ void AmSession::addHandler(AmSessionEventHandler* sess_evh)
 void AmSession::setInput(AmAudio* in)
 {
   lockAudio();
-  input = in;
+  if (streams.size() > 0) streams[0].setSink(in);
+  else {
+    AmRtpAudio *stream = RTPStream();
+    AmAudioPair pair(stream, in, stream, true, false);
+    streams.push_back(pair);
+  }
   unlockAudio();
 }
 
 void AmSession::setOutput(AmAudio* out)
 {
   lockAudio();
-  output = out;
+  if (streams.size() > 1) streams[1].setSource(out);
+  else {
+    AmRtpAudio *stream = RTPStream();
+    if (streams.size() < 1) {
+      // append pair for input from RTP stream
+      AmAudioPair in(stream, NULL, stream, true, false);
+      streams.push_back(in);
+    }
+    // append pair for output to RTP stream
+    AmAudioPair pair(out, stream, stream, false, true);
+    streams.push_back(pair);
+  }
   unlockAudio();
 }
 
 void AmSession::setInOut(AmAudio* in,AmAudio* out)
 {
   lockAudio();
-  input = in;
-  output = out;
+  AmRtpAudio *stream = RTPStream();
+  unsigned cnt = streams.size();
+    
+  if (cnt < 2) {
+    if (cnt < 1) {
+      AmAudioPair ins(stream, in, stream, true, false);
+      streams.push_back(ins);
+    } 
+    else streams[0].setSink(in);
+
+    AmAudioPair outs(out, stream, stream, false, true);
+    streams.push_back(outs);
+  } else {
+    // both streams already exist
+    streams[0].setSink(in);
+    streams[1].setSource(out);
+  }
   unlockAudio();
 }
 
 void AmSession::setLocalInput(AmAudio* in)
 {
+  // replace RTPStream() in input in first member
   lockAudio();
-  local_input = in;
+
+  bool dtmf = false; // do not handle dtmf on local input
+  AmRtpAudio *stream = RTPStream();
+  if (!in) {
+    // unset local input means return back to RTP?
+    in = stream;
+    dtmf = true;
+  }
+
+  if (streams.size() > 0) streams[0].setSource(in);
+  else {
+    AmAudioPair pair(in, NULL, stream, dtmf, false);
+    streams.push_back(pair);
+  }
   unlockAudio();
-}
-
-void AmSession::setLocalOutput(AmAudio* out)
-{
-  lockAudio();
-  local_output = out;
-  unlockAudio();
-}
-
-void AmSession::setLocalInOut(AmAudio* in,AmAudio* out)
-{
-  lockAudio();
-  local_input = in;
-  local_output = out;
-  unlockAudio();
-}
-
-void AmSession::setAudioLocal(unsigned int dir, 
-			      bool local) {
-  assert(dir<2);
-  use_local_audio[dir] = local;
-}
-
-bool AmSession::getAudioLocal(unsigned int dir) { 
-  assert(dir<2); 
-  return use_local_audio[dir]; 
 }
 
 void AmSession::lockAudio()
@@ -780,22 +772,25 @@ void AmSession::onDtmf(int event, int duration_msec)
 void AmSession::clearAudio()
 {
   lockAudio();
-  if(input){
-    input->close();
-    input = 0;
+  AmAudio *in = NULL;
+  AmAudio *out = NULL;
+
+  if (streams.size() > 1) {
+    in = streams[0].getSink();
+    out = streams[1].getSource();
   }
-  if(output){
-    output->close();
-    output = 0;
+  else if (streams.size() > 0) in = streams[0].getSink();
+
+  if (in) {
+    in->close();
+    streams[0].setSink(NULL);
   }
-  if(local_input){
-    local_input->close();
-    local_input = 0;
+  if (out) {
+    out->close();
+    streams[1].setSource(NULL);
   }
-  if(local_output){
-    local_output->close();
-    local_output = 0;
-  }
+  // FIXME: override in WebConferenceDialog and close local_input there ?
+
   unlockAudio();
   DBG("Audio cleared !!!\n");
   postEvent(new AmAudioEvent(AmAudioEvent::cleared));
@@ -1423,6 +1418,28 @@ void AmSession::onZRTPEvent(zrtp_event_t event, zrtp_stream_ctx_t *stream_ctx) {
 }
  
 #endif
+
+int AmSession::processMedia(bool write_streams, unsigned int ts, unsigned char *buffer)
+{
+  int res = 0;
+  AmSession *dtmf_handler = 0;
+
+  lockAudio();
+
+  if (isDtmfDetectionEnabled()) dtmf_handler = this;
+  for (vector<AmAudioPair>::iterator i = streams.begin(); i != streams.end(); ++i) {
+    if (write_streams != i->isOutput()) continue;
+    if (i->process(ts, buffer, dtmf_handler) < 0) {
+      res = -1;
+      break;
+    }
+  }
+
+  unlockAudio();
+
+  return res;
+}
+
 
 /** EMACS **
  * Local variables:

core/AmSession.h
@@ -85,32 +85,20 @@ class AmAudioPair
 
     bool checkInterval(unsigned int ts); 
     bool isOutput() { return is_out; }
-};
 
-class AmSession;
+    /* set/get methods for manipulating internals */
 
-/** class for processing all media streams within one session */
-class AmAudioSession: public AmMediaSession
-{
-  protected:
-    std::vector<AmAudioPair> streams;
-    AmSession *session;
+    AmAudio *getSource() { return src; }
+    AmAudio *getSink() { return sink; }
 
-    int processMedia(bool write_streams, unsigned int ts, unsigned char *buffer);
-
-  public: 
-    AmAudioSession(AmSession *_session): session(_session) { }
-    virtual int readStreams(unsigned int ts, unsigned char *buffer) { return processMedia(false, ts, buffer); }
-    virtual int writeStreams(unsigned int ts, unsigned char *buffer) { return processMedia(true, ts, buffer); }
+    void setSource(AmAudio *_src) { src = _src; }
+    void setSink(AmAudio *_sink) { sink = _sink; }
 };
 
 /** @file AmSession.h */
 
 /* definition imported from Ser parser/msg_parser.h */
 #define FL_FORCE_ACTIVE 2
-
-#define AM_AUDIO_IN  0
-#define AM_AUDIO_OUT 1
 
 
 /**
@@ -125,18 +113,11 @@ class AmSession :
 #endif
   public AmEventQueue, 
   public AmEventHandler,
-  public AmSipDialogEventHandler
+  public AmSipDialogEventHandler,
+  public AmMediaSession
 {
   AmMutex      audio_mut;
-  // remote (to/from RTP) audio inout
-  AmAudio*     input;
-  AmAudio*     output;
 
-  // local (to/from audio dev) audio inout
-  AmAudio*     local_input;
-  AmAudio*     local_output;
-
-  bool use_local_audio[2];
 protected:
   vector<SdpPayload *>  m_payloads;
   //bool         negotiate_onreply;
@@ -229,6 +210,9 @@ protected:
   /** Session event handlers (ex: session timer, UAC auth, etc...) */
   vector<AmSessionEventHandler*> ev_handlers;
 
+  /** media streams to be processed */
+  std::vector<AmAudioPair> streams;
+
 public:
 
   enum SessionRefreshMethod {
@@ -294,12 +278,6 @@ public:
   /** stop processing media - remove from media processor */
   void stopMediaProcessing();
 
-  /** Is the session being processed in  media processor? */
-  bool getProcessingMedia() { return processing_media.get(); }
-
-  /** Is the session detached from media processor? */
-  bool getDetached() { return !processing_media.get(); }
-
   /**
    * Set the call group for this call; calls in the same
    * group are processed by the same media processor thread.
@@ -338,12 +316,12 @@ public:
    * Audio input getter .
    * Note: audio must be locked!
    */
-  AmAudio* getInput() { return input; }
+  AmAudio* getInput() { if (streams.size() > 0) return streams[0].getSink(); else return NULL; }
   /**
    * Audio output getter.
    * Note: audio must be locked!
    */
-  AmAudio* getOutput(){ return output;}
+  AmAudio* getOutput() { if (streams.size() > 1) return streams[1].getSource(); else return NULL; }
 
   /**
    * Audio input & output set methods.
@@ -353,31 +331,11 @@ public:
   void setOutput(AmAudio* out);
   void setInOut(AmAudio* in, AmAudio* out);
 
-
-  /**
-   * Local audio input getter .
-   * Note: audio must be locked!
-   */
-  AmAudio* getLocalInput() { return local_input; }
-  /**
-   * Local audio output getter.
-   * Note: audio must be locked!
-   */
-  AmAudio* getLocalOutput() { return local_output;}
-
   /**
    * Local audio input & output set methods.
    * Note: audio will be locked by the methods.
    */
   void setLocalInput(AmAudio* in);
-  void setLocalOutput(AmAudio* out);
-  void setLocalInOut(AmAudio* in, AmAudio* out);
-
-  /** this switches between local and remote 
-   * audio inout 
-   */
-  void setAudioLocal(unsigned int dir, bool local);
-  bool getAudioLocal(unsigned int dir);
 
   /**
    * Clears input & ouput (no need to lock)
@@ -500,8 +458,6 @@ public:
    * Entry point for DTMF events
    */
   void postDtmfEvent(AmDtmfEvent *);
-
-  void processDtmfEvents();
 
   void setInbandDetector(Dtmf::InbandDetectorType t);
   bool isDtmfDetectionEnabled() { return m_dtmfDetectionEnabled; }
@@ -679,6 +635,17 @@ public:
    * Creates a new Id which can be used within sessions.
    */
   static string getNewId();
+
+  /* ----------------- media processing interface ------------------- */
+
+  protected:
+    int processMedia(bool write_streams, unsigned int ts, unsigned char *buffer);
+
+  public: 
+    virtual int readStreams(unsigned int ts, unsigned char *buffer) { return processMedia(false, ts, buffer); }
+    virtual int writeStreams(unsigned int ts, unsigned char *buffer) { return processMedia(true, ts, buffer); }
+    virtual void clearRTPTimeout() { RTPStream()->clearRTPTimeout(); }
+    virtual void processDtmfEvents();
 
 };
 

apps/webconference/WebConferenceDialog.cpp
@@ -165,8 +165,7 @@ void WebConferenceDialog::onSessionStart() {
       setMute(false);
       DBG("########## dialout: connect to conference '%s' #########\n", dlg.user.c_str()); 
       state = InConference;
-      setAudioLocal(AM_AUDIO_IN, false);
-      setAudioLocal(AM_AUDIO_OUT, false);
+      setLocalInput(NULL);
       time(&connect_ts);
       connectConference(dlg.user);
     }
@@ -184,8 +183,6 @@ void WebConferenceDialog::onRinging(const AmSipReply& rep) {
       RingTone.reset(new AmRingTone(0,2000,4000,440,480)); // US
 
     setLocalInput(RingTone.get());
-    setAudioLocal(AM_AUDIO_IN, true);
-    setAudioLocal(AM_AUDIO_OUT, true);
 
     if (None == state) {
       connectConference(dlg.user);
@@ -200,8 +197,7 @@ void WebConferenceDialog::onEarlySessionStart() {
 
     DBG("########## dialout: connect early session to conference '%s'  #########\n", 
 	dlg.user.c_str());
-    setAudioLocal(AM_AUDIO_IN, false);
-    setAudioLocal(AM_AUDIO_OUT, false);
+    setLocalInput(NULL);
     if (None == state) {
       connectConference(dlg.user);
     }
@@ -422,12 +418,12 @@ void WebConferenceDialog::onMuted(bool mute) {
       
     case InConferenceRinging: {
       if (muted) {
-	setLocalInOut(NULL, NULL);
+	setLocalInput(NULL);
       } else {
 	if(!RingTone.get())
 	  RingTone.reset(new AmRingTone(0,2000,4000,440,480)); // US
     
-	setLocalInOut(RingTone.get(), NULL);
+	setLocalInput(RingTone.get());
 	if (getDetached())
 	  AmMediaProcessor::instance()->addSession(this, callgroup); 
       }

core/AmMediaProcessor.cpp
@@ -40,9 +40,9 @@
 struct SchedRequest :
   public AmEvent
 {
-  AmSession* s;
+  AmMediaSession* s;
 
-  SchedRequest(int id, AmSession* s)
+  SchedRequest(int id, AmMediaSession* s)
     : AmEvent(id), s(s) {}
 };
 
@@ -80,10 +80,10 @@ AmMediaProcessor* AmMediaProcessor::instance()
   return _instance;
 }
 
-void AmMediaProcessor::addSession(AmSession* s, 
+void AmMediaProcessor::addSession(AmMediaSession* s, 
 				  const string& callgroup)
 {
-  s->processing_media.set(true);
+  s->onMediaProcessingStarted();
  
   // evaluate correct scheduler
   unsigned int sched_thread = 0;
@@ -119,24 +119,24 @@ void AmMediaProcessor::addSession(AmSession* s,
     postRequest(new SchedRequest(InsertSession,s));
 }
 
-void AmMediaProcessor::clearSession(AmSession* s) {
+void AmMediaProcessor::clearSession(AmMediaSession* s) {
   removeFromProcessor(s, ClearSession);
 }
 
-void AmMediaProcessor::removeSession(AmSession* s) {
+void AmMediaProcessor::removeSession(AmMediaSession* s) {
   removeFromProcessor(s, RemoveSession);
 }
 
 /* FIXME: implement Call Group ts offsets for soft changing of 
 	call groups 
 */
-void AmMediaProcessor::changeCallgroup(AmSession* s, 
+void AmMediaProcessor::changeCallgroup(AmMediaSession* s, 
 				       const string& new_callgroup) {
   removeFromProcessor(s, SoftRemoveSession);
   addSession(s, new_callgroup);
 }
 
-void AmMediaProcessor::removeFromProcessor(AmSession* s, 
+void AmMediaProcessor::removeFromProcessor(AmMediaSession* s, 
 					   unsigned int r_type) {
   DBG("AmMediaProcessor::removeSession\n");
   group_mut.lock();
@@ -145,7 +145,7 @@ void AmMediaProcessor::removeFromProcessor(AmSession* s,
   unsigned int sched_thread = callgroup2thread[callgroup];
   DBG("  callgroup is '%s', thread %u\n", callgroup.c_str(), sched_thread);
   // erase callgroup membership entry
-  std::multimap<std::string, AmSession*>::iterator it = 
+  std::multimap<std::string, AmMediaSession*>::iterator it = 
     callgroupmembers.lower_bound(callgroup);
   while ((it != callgroupmembers.end()) &&
          (it != callgroupmembers.upper_bound(callgroup))) {
@@ -266,10 +266,10 @@ void AmMediaProcessorThread::run()
  */
 void AmMediaProcessorThread::processDtmfEvents()
 {
-  for(set<AmSession*>::iterator it = sessions.begin();
+  for(set<AmMediaSession*>::iterator it = sessions.begin();
       it != sessions.end(); it++)
     {
-      AmSession* s = (*it);
+      AmMediaSession* s = (*it);
       s->processDtmfEvents();
     }
 }
@@ -277,57 +277,20 @@ void AmMediaProcessorThread::processDtmfEvents()
 void AmMediaProcessorThread::processAudio(unsigned int ts)
 {
   // receiving
-  for(set<AmSession*>::iterator it = sessions.begin();
-      it != sessions.end(); it++){
-
-    AmSession* s = (*it);
-    unsigned int f_size = s->RTPStream()->getFrameSize(); 
-    s->lockAudio();
-
-    int got_audio = s->RTPStream()->get(ts,buffer,f_size);
-    if (got_audio < 0) {
-      postRequest(new SchedRequest(AmMediaProcessor::ClearSession,s));
-    }
-    else if (got_audio > 0) { // FIXME: what about got_audio == 0 ?
-      if (s->isDtmfDetectionEnabled())
-        s->putDtmfAudio(buffer, got_audio, ts);
-
-    // process received audio
-      AmAudio* input = s->getInput();
-      if (input) {
-        int ret = input->put(ts,buffer,got_audio);
-        if(ret < 0){
-          DBG("input->put() returned: %i\n",ret);
-          postRequest(new SchedRequest(AmMediaProcessor::ClearSession,s));
-        }
-      }
-    }
-
-    s->unlockAudio();
+  for(set<AmMediaSession*>::iterator it = sessions.begin();
+      it != sessions.end(); it++)
+  {
+    if ((*it)->readStreams(ts, buffer) < 0)
+      postRequest(new SchedRequest(AmMediaProcessor::ClearSession, *it));
   }
 
   // sending
-  for(set<AmSession*>::iterator it = sessions.begin();
-      it != sessions.end(); it++){
-
-    AmSession* s = (*it);
-    s->lockAudio();
-    AmAudio* output = s->getOutput();
-
-    if(output && s->RTPStream()->sendIntReached()){
-
-      int size = output->get(ts,buffer,s->RTPStream()->getFrameSize());
-      if(size <= 0){
-        DBG("output->get() returned: %i\n",size);
-        postRequest(new SchedRequest(AmMediaProcessor::ClearSession,s)); 
-      } else {
-        // audio should go to RTP
-        if(s->RTPStream()->put(ts,buffer,size)<0)
-          postRequest(new SchedRequest(AmMediaProcessor::ClearSession,s));
-      }
-    }
-    s->unlockAudio();
-  }	
+  for(set<AmMediaSession*>::iterator it = sessions.begin();
+      it != sessions.end(); it++)
+  {
+    if ((*it)->writeStreams(ts, buffer) < 0)
+      postRequest(new SchedRequest(AmMediaProcessor::ClearSession, *it));
+  }
 }
 
 void AmMediaProcessorThread::process(AmEvent* e)
@@ -343,27 +306,27 @@ void AmMediaProcessorThread::process(AmEvent* e)
   case AmMediaProcessor::InsertSession:
     DBG("Session inserted to the scheduler\n");
     sessions.insert(sr->s);
-    sr->s->RTPStream()->clearRTPTimeout();
+    sr->s->clearRTPTimeout();
     break;
 
   case AmMediaProcessor::RemoveSession:{
-    AmSession* s = sr->s;
-    set<AmSession*>::iterator s_it = sessions.find(s);
+    AmMediaSession* s = sr->s;
+    set<AmMediaSession*>::iterator s_it = sessions.find(s);
     if(s_it != sessions.end()){
       sessions.erase(s_it);
-      s->processing_media.set(false);
+      s->onMediaProcessingTerminated();
       DBG("Session removed from the scheduler\n");
     }
   }
     break;
 
   case AmMediaProcessor::ClearSession:{
-    AmSession* s = sr->s;
-    set<AmSession*>::iterator s_it = sessions.find(s);
+    AmMediaSession* s = sr->s;
+    set<AmMediaSession*>::iterator s_it = sessions.find(s);
     if(s_it != sessions.end()){
       sessions.erase(s_it);
       s->clearAudio();
-      s->processing_media.set(false);
+      s->onMediaProcessingTerminated();
       DBG("Session removed from the scheduler\n");
     }
   }
@@ -371,8 +334,8 @@ void AmMediaProcessorThread::process(AmEvent* e)
 
 
   case AmMediaProcessor::SoftRemoveSession:{
-    AmSession* s = sr->s;
-    set<AmSession*>::iterator s_it = sessions.find(s);
+    AmMediaSession* s = sr->s;
+    set<AmMediaSession*>::iterator s_it = sessions.find(s);
     if(s_it != sessions.end()){
       sessions.erase(s_it);
       DBG("Session removed softly from the scheduler\n");

core/AmMediaProcessor.h
@@ -30,7 +30,6 @@
 
 #include "AmEventQueue.h"
 #include "amci/amci.h" // AUDIO_BUFFER_SIZE
-class AmSession;
 
 #include <set>
 using std::set;
@@ -40,14 +39,32 @@ struct SchedRequest;
 
 class AmMediaSession
 {
+  private:
+    AmCondition<bool> processing_media;
+
   public:
+    AmMediaSession(): processing_media(false) { }
+
     /* first read from all RTP streams */
-    virtual int readStreams(bool output, unsigned int ts, unsigned char *buffer) = 0;
+    virtual int readStreams(unsigned int ts, unsigned char *buffer) = 0;
     
     /* after read write to all RTP streams */
-    virtual int writeStreams(bool output, unsigned int ts, unsigned char *buffer) = 0;
+    virtual int writeStreams(unsigned int ts, unsigned char *buffer) = 0;
+
+    virtual void processDtmfEvents() = 0;
+    virtual void clearAudio() = 0;
+    virtual void clearRTPTimeout() = 0;
 
     virtual ~AmMediaSession() { }
+
+    void onMediaProcessingStarted() { processing_media.set(true); }
+    void onMediaProcessingTerminated() { processing_media.set(false); }
+  
+    /** Is the session being processed in  media processor? */
+    bool getProcessingMedia() { return processing_media.get(); }
+  
+    /** Is the session detached from media processor? */
+    bool getDetached() { return !processing_media.get(); }
 };
 
 /**
@@ -63,7 +80,7 @@ class AmMediaProcessorThread :
 {
   AmEventQueue    events;
   unsigned char   buffer[AUDIO_BUFFER_SIZE];
-  set<AmSession*> sessions;
+  set<AmMediaSession*> sessions;
   
   void processAudio(unsigned int ts);
   /**
@@ -103,14 +120,14 @@ class AmMediaProcessor
   AmMediaProcessorThread**  threads;
   
   std::map<string, unsigned int> callgroup2thread;
-  std::multimap<string, AmSession*> callgroupmembers;
-  std::map<AmSession*, string> session2callgroup;
+  std::multimap<string, AmMediaSession*> callgroupmembers;
+  std::map<AmMediaSession*, string> session2callgroup;
   AmMutex group_mut;
 
   AmMediaProcessor();
   ~AmMediaProcessor();
 	
-  void removeFromProcessor(AmSession* s, unsigned int r_type);
+  void removeFromProcessor(AmMediaSession* s, unsigned int r_type);
 public:
   /** 
    * InsertSession     : inserts the session to the processor
@@ -124,13 +141,13 @@ public:
 
   void init();
   /** Add session s to processor */
-  void addSession(AmSession* s, const string& callgroup);
+  void addSession(AmMediaSession* s, const string& callgroup);
   /** Remove session s from processor */
-  void removeSession(AmSession* s);
+  void removeSession(AmMediaSession* s);
   /** Remove session s from processor and clear its audio */
-  void clearSession(AmSession* s);
+  void clearSession(AmMediaSession* s);
   /** Change the callgroup of a session (use with caution) */
-  void changeCallgroup(AmSession* s, 
+  void changeCallgroup(AmMediaSession* s, 
 		       const string& new_callgroup);
 
   void stop();

core/AmRtpAudio.cpp
@@ -118,9 +118,6 @@ int AmRtpAudio::get(unsigned int ref_ts, unsigned char* buffer, unsigned int nb_
 {
   if (!(receiving || getPassiveMode())) return 0; // like nothing received
     
-  // complete frame time reached? 
-  if (!checkInterval(ref_ts, nb_samples)) return 0; // like nothing received
-
   int ret = receive(ref_ts);
   if(ret < 0){
     switch(ret){

commit 687a7c4cc2640ca530c8bcef5e8a219c5e3b401f

b/f: AmSession - removed duplicate condition processing_media
fixes bug introduced in commit 263845ad5d607a37b004e62a9959a0406a784c3f

core/AmSession.cpp
@@ -106,7 +106,6 @@ struct timeval cps_max_timestamp = avg_last_timestamp;
 AmSession::AmSession()
   : AmEventQueue(this),
     dlg(this),
-    processing_media(false),
     input(NULL), output(NULL),
     sess_stopped(false),
     m_dtmfDetector(this), m_dtmfEventQueue(&m_dtmfDetector),
@@ -156,7 +155,7 @@ void AmSession::changeCallgroup(const string& cg) {
 
 void AmSession::startMediaProcessing() 
 {
-  if(getStopped() || processing_media.get())
+  if(getStopped() || getProcessingMedia())
     return;
 
   if(isAudioSet()) {
@@ -170,7 +169,7 @@ void AmSession::startMediaProcessing()
 
 void AmSession::stopMediaProcessing() 
 {
-  if(!processing_media.get())
+  if(!getProcessingMedia())
     return;
 
   AmMediaProcessor::instance()->removeSession(this);

core/AmSession.h
@@ -168,7 +168,6 @@ private:
 
 
   AmCondition<bool> sess_stopped;
-  AmCondition<bool> processing_media;
 
   static void session_started();
   static void session_stopped();