ngvoice/sems-amr

commit 3d6fc20162815c4a92f48eddef8ce6e029af2c10

AmSession: simplified media processing
Basically returned to previous implementation with input/output and one stream.
Derrived classes (AmB2BSession) should override readStreams/writeStreams based
on their needs.

Local input handling moved to WebConferenceDialog because it was used just there.

apps/webconference/WebConferenceDialog.cpp
@@ -434,3 +434,53 @@ void WebConferenceDialog::onMuted(bool mute) {
   }
 }
 
+void WebConferenceDialog::setLocalInput(AmAudio* in)
+{
+  lockAudio();
+  local_input = in;
+  unlockAudio();
+}
+
+int WebConferenceDialog::readStreams(unsigned int ts, unsigned char *buffer) 
+{ 
+  int res = 0;
+  lockAudio();
+
+  AmRtpAudio *stream = RTPStream();
+  unsigned int f_size = stream->getFrameSize();
+  if (stream->checkInterval(ts, f_size)) {
+    int got = 0;
+    if (local_input) got = local_input->get(ts, buffer, f_size);
+    else got = stream->get(ts, buffer, f_size);
+    if (got < 0) res = -1;
+    if (got > 0) {
+      if (isDtmfDetectionEnabled())
+        putDtmfAudio(buffer, got, ts);
+
+      if (input) res = input->put(ts, buffer, got);
+    }
+  }
+  
+  unlockAudio();
+  return res;
+}
+
+bool WebConferenceDialog::isAudioSet()
+{
+  lockAudio();
+  bool set = input || output || local_input;
+  unlockAudio();
+  return set;
+}
+
+void WebConferenceDialog::clearAudio()
+{
+  lockAudio();
+  if (local_input) {
+    local_input->close();
+    local_input = NULL;
+  }
+  unlockAudio();
+  AmSession::clearAudio(); // locking second time but called not so often?
+}
+

core/AmSession.cpp
@@ -158,11 +158,7 @@ void AmSession::startMediaProcessing()
   if(getStopped() || processing_media.get())
     return;
 
-  bool in_out_set;
-  lockAudio();
-  in_out_set = streams.size() > 0;
-  unlockAudio();
-  if(in_out_set) {
+  if(isAudioSet()) {
     AmMediaProcessor::instance()->addSession(this, callgroup);
   }
   else {
@@ -188,75 +184,31 @@ void AmSession::addHandler(AmSessionEventHandler* sess_evh)
 void AmSession::setInput(AmAudio* in)
 {
   lockAudio();
-  if (streams.size() > 0) streams[0].setSink(in);
-  else {
-    AmRtpAudio *stream = RTPStream();
-    AmAudioPair pair(stream, in, stream, true, false);
-    streams.push_back(pair);
-  }
+  input = in;
   unlockAudio();
 }
 
 void AmSession::setOutput(AmAudio* out)
 {
   lockAudio();
-  if (streams.size() > 1) streams[1].setSource(out);
-  else {
-    AmRtpAudio *stream = RTPStream();
-    if (streams.size() < 1) {
-      // append pair for input from RTP stream
-      AmAudioPair in(stream, NULL, stream, true, false);
-      streams.push_back(in);
-    }
-    // append pair for output to RTP stream
-    AmAudioPair pair(out, stream, stream, false, true);
-    streams.push_back(pair);
-  }
+  output = out;
   unlockAudio();
 }
 
 void AmSession::setInOut(AmAudio* in,AmAudio* out)
 {
   lockAudio();
-  AmRtpAudio *stream = RTPStream();
-  unsigned cnt = streams.size();
-    
-  if (cnt < 2) {
-    if (cnt < 1) {
-      AmAudioPair ins(stream, in, stream, true, false);
-      streams.push_back(ins);
-    } 
-    else streams[0].setSink(in);
-
-    AmAudioPair outs(out, stream, stream, false, true);
-    streams.push_back(outs);
-  } else {
-    // both streams already exist
-    streams[0].setSink(in);
-    streams[1].setSource(out);
-  }
+  input = in;
+  output = out;
   unlockAudio();
 }
-
-void AmSession::setLocalInput(AmAudio* in)
+  
+bool AmSession::isAudioSet()
 {
-  // replace RTPStream() in input in first member
   lockAudio();
-
-  bool dtmf = false; // do not handle dtmf on local input
-  AmRtpAudio *stream = RTPStream();
-  if (!in) {
-    // unset local input means return back to RTP?
-    in = stream;
-    dtmf = true;
-  }
-
-  if (streams.size() > 0) streams[0].setSource(in);
-  else {
-    AmAudioPair pair(in, NULL, stream, dtmf, false);
-    streams.push_back(pair);
-  }
+  bool set = input || output;
   unlockAudio();
+  return set;
 }
 
 void AmSession::lockAudio()
@@ -772,24 +724,15 @@ void AmSession::onDtmf(int event, int duration_msec)
 void AmSession::clearAudio()
 {
   lockAudio();
-  AmAudio *in = NULL;
-  AmAudio *out = NULL;
 
-  if (streams.size() > 1) {
-    in = streams[0].getSink();
-    out = streams[1].getSource();
+  if (input) {
+    input->close();
+    input = NULL;
   }
-  else if (streams.size() > 0) in = streams[0].getSink();
-
-  if (in) {
-    in->close();
-    streams[0].setSink(NULL);
+  if (output) {
+    output->close();
+    output = NULL;
   }
-  if (out) {
-    out->close();
-    streams[1].setSource(NULL);
-  }
-  // FIXME: override in WebConferenceDialog and close local_input there ?
 
   unlockAudio();
   DBG("Audio cleared !!!\n");
@@ -1419,27 +1362,46 @@ void AmSession::onZRTPEvent(zrtp_event_t event, zrtp_stream_ctx_t *stream_ctx) {
  
 #endif
 
-int AmSession::processMedia(bool write_streams, unsigned int ts, unsigned char *buffer)
-{
+int AmSession::readStreams(unsigned int ts, unsigned char *buffer) 
+{ 
   int res = 0;
-  AmSession *dtmf_handler = 0;
-
   lockAudio();
 
-  if (isDtmfDetectionEnabled()) dtmf_handler = this;
-  for (vector<AmAudioPair>::iterator i = streams.begin(); i != streams.end(); ++i) {
-    if (write_streams != i->isOutput()) continue;
-    if (i->process(ts, buffer, dtmf_handler) < 0) {
-      res = -1;
-      break;
+  AmRtpAudio *stream = RTPStream();
+  unsigned int f_size = stream->getFrameSize();
+  if (stream->checkInterval(ts, f_size)) {
+    int got = stream->get(ts, buffer, f_size);
+    if (got < 0) res = -1;
+    if (got > 0) {
+      if (isDtmfDetectionEnabled())
+        putDtmfAudio(buffer, got, ts);
+
+      if (input) res = input->put(ts, buffer, got);
     }
   }
-
+  
   unlockAudio();
-
   return res;
 }
 
+int AmSession::writeStreams(unsigned int ts, unsigned char *buffer) 
+{ 
+  int res = 0;
+  lockAudio();
+
+  AmRtpAudio *stream = RTPStream();
+  if (stream->sendIntReached()) { // FIXME: shouldn't depend on checkInterval call before!
+    unsigned int f_size = stream->getFrameSize();
+    int got = 0;
+    if (output) got = output->get(ts, buffer, f_size);
+    if (got < 0) res = -1;
+    if (got > 0) res = stream->put(ts, buffer, got);
+  }
+  
+  unlockAudio();
+  return res;
+
+}
 
 /** EMACS **
  * Local variables:

apps/webconference/WebConferenceDialog.h
@@ -86,6 +86,9 @@ private:
   // ID from X-ParticipantID header
   string participant_id;
 
+  AmAudio *local_input;
+  void setLocalInput(AmAudio* in);
+
 public:
   WebConferenceDialog(AmPromptCollection& prompts,
 		      WebConferenceFactory* my_f, 
@@ -109,6 +112,10 @@ public:
 
   UACAuthCred* getCredentials() { return cred; }
 
+  // overriden media processing (local_input)
+  virtual int readStreams(unsigned int ts, unsigned char *buffer);
+  virtual bool isAudioSet();
+  virtual void clearAudio();
 };
 
 #endif

core/AmSession.h
@@ -210,8 +210,7 @@ protected:
   /** Session event handlers (ex: session timer, UAC auth, etc...) */
   vector<AmSessionEventHandler*> ev_handlers;
 
-  /** media streams to be processed */
-  std::vector<AmAudioPair> streams;
+  AmAudio *input, *output;
 
 public:
 
@@ -316,12 +315,12 @@ public:
    * Audio input getter .
    * Note: audio must be locked!
    */
-  AmAudio* getInput() { if (streams.size() > 0) return streams[0].getSink(); else return NULL; }
+  AmAudio* getInput() { return input; }
   /**
    * Audio output getter.
    * Note: audio must be locked!
    */
-  AmAudio* getOutput() { if (streams.size() > 1) return streams[1].getSource(); else return NULL; }
+  AmAudio* getOutput() { return output; }
 
   /**
    * Audio input & output set methods.
@@ -331,16 +330,13 @@ public:
   void setOutput(AmAudio* out);
   void setInOut(AmAudio* in, AmAudio* out);
 
-  /**
-   * Local audio input & output set methods.
-   * Note: audio will be locked by the methods.
-   */
-  void setLocalInput(AmAudio* in);
+  /** checks if input/output is set, might be overidden! */
+  virtual bool isAudioSet();
 
   /**
    * Clears input & ouput (no need to lock)
    */
-  void clearAudio();
+  virtual void clearAudio();
 
   /** setter for rtp_str->mute */
   void setMute(bool mute) { RTPStream()->mute = mute; }
@@ -638,12 +634,9 @@ public:
 
   /* ----------------- media processing interface ------------------- */
 
-  protected:
-    int processMedia(bool write_streams, unsigned int ts, unsigned char *buffer);
-
   public: 
-    virtual int readStreams(unsigned int ts, unsigned char *buffer) { return processMedia(false, ts, buffer); }
-    virtual int writeStreams(unsigned int ts, unsigned char *buffer) { return processMedia(true, ts, buffer); }
+    virtual int readStreams(unsigned int ts, unsigned char *buffer);
+    virtual int writeStreams(unsigned int ts, unsigned char *buffer);
     virtual void clearRTPTimeout() { RTPStream()->clearRTPTimeout(); }
     virtual void processDtmfEvents();
 

commit 25d840d1843decab2e175a4947e240aa24c44002

b/f: added missing input/output/local_input initialization
fixes bug introduced by commit 3d6fc20162815c4a92f48eddef8ce6e029af2c10

apps/webconference/WebConferenceDialog.cpp
@@ -34,7 +34,8 @@ WebConferenceDialog::WebConferenceDialog(AmPromptCollection& prompts,
 					 UACAuthCred* cred)
   : play_list(this), separator(this, 0), prompts(prompts), state(None),
     factory(my_f), cred(cred), muted(false), 
-    connect_ts(-1), disconnect_ts(-1) 
+    connect_ts(-1), disconnect_ts(-1),
+    local_input(NULL)
 {
   // with SIP credentials -> outgoing ('dialout') call
   is_dialout = (cred != NULL);

core/AmSession.cpp
@@ -107,6 +107,7 @@ AmSession::AmSession()
   : AmEventQueue(this),
     dlg(this),
     processing_media(false),
+    input(NULL), output(NULL),
     sess_stopped(false),
     m_dtmfDetector(this), m_dtmfEventQueue(&m_dtmfDetector),
     m_dtmfDetectionEnabled(true),